ebf4e171e0da2758f3ddc6dacc4465768916b847
Edvin Lundqvist Sternvik <e.sternvik@gmail.com>
2019-03-28 16:04:34 +0100

    Implemented reflections on walls

--- OpenGLRaytracing/src/ComputeShader.glsl
+++ OpenGLRaytracing/src/ComputeShader.glsl
@@ -33,8 +33,19 @@ hitInfo castRay(vec3 origin, vec3 rayDir, float maxDistance, float increment) {
 	return hitInf;
 }
 
+float calculateColor(hitInfo info) {
+	float color = 0;
+	if (info.hit) {
+		float lightDistance = distance(info.hitPoint, lightPos[0]);
+		vec3 rayDir2 = normalize(lightPos[0] - info.hitPoint);
+		hitInfo info2 = castRay(info.hitPoint + rayDir2 * 0.02f, rayDir2, lightDistance, 0.01f);
+		color = 1 / lightDistance;
+		if (info2.hit) color *= 0.5f;
+	}
+	return color;
+}
+
 void main() {
-	vec4 pixel = vec4(0, 0, 0, 1.0);
 	ivec2 dims = imageSize(imgOutput);
 	ivec2 pixelCoords = ivec2(gl_GlobalInvocationID.xy);
 
@@ -42,29 +53,33 @@ void main() {
 
 	float angle = x * -0.698f + lookDir;
 	vec3 rayDir = vec3(sin(angle), 0, cos(angle));
+
 	hitInfo info = castRay(pos, rayDir, 50, 0.01f);
-	float lightDistance = 0, color;
+	float color = calculateColor(info); 
+	
+	vec3 refDir = rayDir;
+	vec3 normal = (info.hitPoint - ivec3(info.hitPoint)) - vec3(0.5, 0, 0.5);
+	if(abs(normal.x) > abs(normal.z)) refDir.x *= -1;
+	else refDir.z *= -1;
 
-	if (info.hit) {
-		lightDistance = distance(info.hitPoint, lightPos[0]);
-		vec3 rayDir2 = normalize(lightPos[0] - info.hitPoint);
-		hitInfo info2 = castRay(info.hitPoint + rayDir2 * 0.02f, rayDir2, lightDistance, 0.01f);
-		color = 1 / lightDistance;
-		pixel = vec4(color, color, color, 1);
-		if (info2.hit) pixel *= 0.5f;
-	}
+	hitInfo refInfo = castRay(info.hitPoint, refDir, 50.0f, 0.01f);
+	float color2 = calculateColor(refInfo);
+	color += color2 * 0.1f;
 
+	vec4 pixel = vec4(color, color, color, 1);
+		
+	
 	int hHeight = dims.y / 2;
 	int height = hHeight - int(float(hHeight) - (float(dims.y) / info.dist));
 	for (int i = 0; i < hHeight; ++i) {
 		if (i > height) {
-			float floorColor = (0.5 - lightDistance * 0.04f) * color;
+			float floorColor = color * 0.4f;
 			pixel = vec4(floorColor, floorColor, floorColor, 1);
 		}
 		pixelCoords.y = hHeight - i;
 		imageStore(imgOutput, pixelCoords, pixel);
 		if (i > height) {
-			pixel *= 0.5f;
+			pixel *= 0.3;
 		}
 		pixelCoords.y = hHeight + i;
 		imageStore(imgOutput, pixelCoords, pixel);
